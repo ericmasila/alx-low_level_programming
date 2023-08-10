bb4c59ba2d60585ef59905c2c95421d7c7a81058
ericmasila <ericmasila254@gmail.com>
2023-08-10 01:50:40 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- *main -entry point
+ * main - Entry point
  *
- *return (always zero)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
